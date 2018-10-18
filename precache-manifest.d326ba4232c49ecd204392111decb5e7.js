be6137de27981ab072c3dfe6affe3041b8f67dd4
AnastasiaBro <a-sirion@yandex.ru>
2018-10-18 22:37:21 +0300

    Updates

---
+++ precache-manifest.d326ba4232c49ecd204392111decb5e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d29f11f8efc180625eb",
+    "url": "/example/static/js/runtime~main.4d29f11f.js"
+  },
+  {
+    "revision": "555a1a3aa914a152f122",
+    "url": "/example/static/js/main.555a1a3a.chunk.js"
+  },
+  {
+    "revision": "fe93de5891fecb5b8dd0",
+    "url": "/example/static/js/1.fe93de58.chunk.js"
+  },
+  {
+    "revision": "555a1a3aa914a152f122",
+    "url": "/example/static/css/main.2ce98aac.chunk.css"
+  },
+  {
+    "revision": "c817cc9609fe48e043d4703d78673914",
+    "url": "/example/index.html"
+  }
+];
